26ad41a499591793fe121672c630fdaec70958a4
André "Dezzy <andrevcv1@gmail.com>
2021-08-09 00:23:00 -0300

    chore: Improve link tree page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { NextSeo } from 'next-seo'
+import Image from 'next/image'
 
-import styles from '../styles/Home.module.css'
+import avatarImg from '@/../public/avatar.jpg'
+
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -11,11 +14,20 @@ export default function Home() {
       />
 
       <main className={styles.main}>
-        <h1 className={styles.title}>André "Dezzy" Victor</h1>
+        <div className={styles.avatar}>
+          <Image
+            alt="André's Avatar Photo"
+            height={132}
+            src={avatarImg}
+            width={132}
+          />
+        </div>
 
-        <button className={styles['tree-button']}>
+        <button className={styles.button}>
           Co-Founder & CTO at Payme Trade
         </button>
+
+        <button className={styles.button}>LinkedIn</button>
       </main>
     </div>
   )
