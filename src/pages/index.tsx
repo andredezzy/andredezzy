cb160ac290769ea66feea3f5584e8dd4647e5044
André "Dezzy <andrevcv1@gmail.com>
2021-08-09 11:41:10 -0300

    feat: Add icon & linking to buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextSeo } from 'next-seo'
 import Image from 'next/image'
+import Link from 'next/link'
 
-import avatarImg from '@/../public/avatar.jpg'
+import avatarImg from '@public/avatar.jpg'
+import paymeTradeIcon from '@public/icons/payme-trade.svg'
+import React from 'react'
+import { FiLinkedin, FiGithub } from 'react-icons/fi'
 
 import styles from '../styles/Home.module.scss'
 
@@ -23,11 +27,32 @@ export default function Home() {
           />
         </div>
 
-        <button className={styles.button}>
-          Co-Founder & CTO at Payme Trade
-        </button>
+        <Link href="https://paymetrade.com/">
+          <a className={styles.button} target="_blank">
+            <i>
+              <Image alt="Payme Trade's Icon" src={paymeTradeIcon} />
+            </i>
+            Co-Founder & CTO at Payme Trade
+          </a>
+        </Link>
 
-        <button className={styles.button}>LinkedIn</button>
+        <Link href="https://www.linkedin.com/in/andredezzy/">
+          <a className={styles.button} target="_blank">
+            <i>
+              <FiLinkedin />
+            </i>
+            LinkedIn
+          </a>
+        </Link>
+
+        <Link href="https://github.com/andredezzy">
+          <a className={styles.button} target="_blank">
+            <i>
+              <FiGithub />
+            </i>
+            GitHub
+          </a>
+        </Link>
       </main>
     </div>
   )
