b5739625c206aec83438f925107f0b6a4f9b2cc3
André "Dezzy" Victor <hello@andredezzy.com>
2023-12-13 13:03:35 -0300

    Update page layout and heading style

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { ThemeSwitcher } from './components/theme-switcher';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>André &quot;Dezzy&quot; Victor</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24">
+      <h1 className="text-2xl font-bold">André &quot;Dezzy&quot; Victor</h1>
 
       <ThemeSwitcher />
     </main>
