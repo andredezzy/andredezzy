92ae6694bae4f2009e28ac443a1185357708d6b1
André "Dezzy" Victor <hello@andredezzy.com>
2023-12-16 20:05:13 -0300

    Add dark mode support to ArticleNotionRenderer

--- app/(ui)/articles/[slug]/article-notion-renderer.tsx
+++ app/(ui)/articles/[slug]/article-notion-renderer.tsx
@@ -3,6 +3,8 @@
 import { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
 
+import { useUserPreferences } from '@/features/user-preferences/user-preferences-context';
+
 interface ArticleNotionRendererProps {
   recordMap: ExtendedRecordMap;
 }
@@ -10,7 +12,13 @@ interface ArticleNotionRendererProps {
 export function ArticleNotionRenderer({
   recordMap,
 }: ArticleNotionRendererProps) {
+  const { theme } = useUserPreferences();
+
   return (
-    <NotionRenderer recordMap={recordMap} className="!mx-0 !w-full !px-0" />
+    <NotionRenderer
+      recordMap={recordMap}
+      className="!mx-0 !w-full !px-0"
+      darkMode={theme === 'dark'}
+    />
   );
 }
