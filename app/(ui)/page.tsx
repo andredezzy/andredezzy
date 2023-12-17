e0fec8d054c874e2a6d75d8fa8121b0cf8463ef5
André "Dezzy" Victor <hello@andredezzy.com>
2023-12-17 14:58:01 -0300

    Refactor UI layout and styling in page component

--- app/(ui)/page.tsx
+++ app/(ui)/page.tsx
@@ -25,41 +25,54 @@ export default async function HomePage() {
   const articles = response.results as NotionArticle[];
 
   return (
-    <main className="container min-h-screen space-y-6 py-20">
-      <h1 className="font-serif text-3xl font-semibold">
-        André &quot;Dezzy&quot; Victor
-      </h1>
+    <main className="container min-h-screen space-y-8 py-20">
+      <header className="space-y-6">
+        <h1 className="font-serif text-3xl font-semibold">
+          André &quot;Dezzy&quot; Victor
+        </h1>
 
-      <p className="text-muted-foreground">
-        Software Engineer. Designer. DevTools & Open Source enthusiast.
-        Entrepreneur. Cars amateur. Photographer. Traveling and (trying)
-        fitness/bodybuilding lifestyle.
-      </p>
+        <p className="text-muted-foreground">
+          Software Engineer. Designer. DevTools & Open Source enthusiast.
+          Entrepreneur. Cars amateur. Photographer. Traveling and (trying)
+          fitness/bodybuilding lifestyle.
+        </p>
 
-      <h2 className="text-lg font-medium">Articles</h2>
+        <Link
+          className="block text-indigo-500 underline"
+          href="https://notion.andredezzy.com"
+        >
+          Resume on Notion
+        </Link>
+      </header>
 
-      <ul className="flex flex-col gap-2">
-        {articles.map(article => {
-          const slug = '/articles' + new URL(article.url).pathname;
+      <hr />
 
-          return (
-            <li key={article.id}>
-              <Link
-                className={cn('underline', {
-                  'text-muted-foreground':
-                    article.properties.Status.status.name !== 'Published',
-                  'text-indigo-500 dark:text-indigo-400':
-                    article.properties.Status.status.name === 'Published',
-                })}
-                href={slug}
-              >
-                {article.properties.Name.title[0].plain_text} (
-                {article.properties.Status.status.name})
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <main className="space-y-6">
+        <h2 className="text-lg font-medium">Articles</h2>
+
+        <ul className="flex flex-col gap-2">
+          {articles.map(article => {
+            const slug = '/articles' + new URL(article.url).pathname;
+
+            return (
+              <li key={article.id}>
+                <Link
+                  className={cn('underline', {
+                    'text-muted-foreground':
+                      article.properties.Status.status.name !== 'Published',
+                    'text-indigo-500 dark:text-indigo-400':
+                      article.properties.Status.status.name === 'Published',
+                  })}
+                  href={slug}
+                >
+                  {article.properties.Name.title[0].plain_text} (
+                  {article.properties.Status.status.name})
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
 
       <ThemeSwitcher />
     </main>
