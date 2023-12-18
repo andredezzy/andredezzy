699161fa0a2ed835eab0c738729a722161431cbc
André "Dezzy" Victor <hello@andredezzy.com>
2023-12-18 12:13:16 -0300

    Add redirects for articles to blog

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: '/articles/:path*',
+        destination: '/blog/:path*',
+        permanent: true,
+      },
+    ];
+  },
+
   headers() {
     return [
       {
